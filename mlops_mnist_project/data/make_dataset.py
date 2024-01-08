124fa310e6c8a15269062896565e357f83aeb080
augustsemrau <augu0093@gmail.com>
2024-01-08 11:52:05 +0100

    Code works

--- mlops_mnist_project/data/make_dataset.py
+++ mlops_mnist_project/data/make_dataset.py
@@ -1,3 +1,63 @@
-if __name__ == '__main__':
+import os
+import torch
+
+
+def create_dataset(image_files, target_files, data_path):
+    """
+    Function to create a dataset from the given list of file names
+    """
+    image_tensors = torch.cat([torch.load(os.path.join(data_path, file_name)) for file_name in image_files])
+    target_tensors = torch.cat([torch.load(os.path.join(data_path, file_name)) for file_name in target_files])
+
+    return image_tensors, target_tensors
+
+
+def mnist():
+    """Return train and test dataloaders for MNIST."""
+    data_path = os.path.join(os.path.dirname(__file__), "..", "..", "data", "raw", "corruptmnist")
+
+    # Create file name lists
+    train_image_files = [f"train_images_{i}.pt" for i in range(6)]
+    train_target_files = [f"train_target_{i}.pt" for i in range(6)]
+    test_image_files = ["test_images.pt"]
+    test_target_files = ["test_target.pt"]
+
+    # Create datasets
+    train_image_tensors, train_target_tensors = create_dataset(train_image_files, train_target_files, data_path)
+    test_image_tensors, test_target_tensors = create_dataset(test_image_files, test_target_files, data_path)
+
+    return train_image_tensors, train_target_tensors, test_image_tensors, test_target_tensors
+
+
+def process(data):
+    """
+    Function to process the data by normalizing it
+    """
+    # Normalize the data
+    mean = data.mean()
+    std = data.std()
+    data = (data - mean) / std
+
+    return data
+
+
+if __name__ == "__main__":
     # Get the data and process it
-    pass
+    train_image_tensors, train_target_tensors, test_image_tensors, test_target_tensors = mnist()
+
+    # Save raw data as tensor dataset
+    base_path = os.path.join("data", "raw")
+    torch.save(train_image_tensors, os.path.join(base_path, "train_images.pt"))
+    torch.save(train_target_tensors, os.path.join(base_path, "train_targets.pt"))
+    torch.save(test_image_tensors, os.path.join(base_path, "test_images.pt"))
+    torch.save(test_target_tensors, os.path.join(base_path, "test_targets.pt"))
+
+    # Process the data
+    train_image_tensors, test_image_tensors = process(train_image_tensors), process(test_image_tensors)
+
+    # Save processed data
+    base_path = os.path.join("data", "processed")
+    torch.save(train_image_tensors, os.path.join(base_path, "train_images.pt"))
+    torch.save(train_target_tensors, os.path.join(base_path, "train_targets.pt"))
+    torch.save(test_image_tensors, os.path.join(base_path, "test_images.pt"))
+    torch.save(test_target_tensors, os.path.join(base_path, "test_targets.pt"))
---
+++ mlops_mnist_project/train_model.py
@@ -0,0 +1,74 @@
+import os
+
+import matplotlib.pyplot as plt
+import torch
+from tqdm import tqdm
+
+from models.model import MyNeuralNet
+from torch.utils.data import DataLoader, Dataset
+
+
+
+class CustomDataset(Dataset):
+    def __init__(self, image_tensors, target_tensors):
+        self.images = image_tensors
+        self.labels = target_tensors
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        x = self.images[idx].unsqueeze(0)
+        y = self.labels[idx]
+        return x, y
+
+
+def train(lr):
+    """Train a model on MNIST."""
+    print("Training day and night")
+    print(lr)
+
+    model = MyNeuralNet()
+
+    base_path = os.path.join("data", "processed")
+    train_image_tensors = torch.load(os.path.join(base_path, "train_images.pt"))
+    train_target_tensors = torch.load(os.path.join(base_path, "train_targets.pt"))
+    train_set = CustomDataset(train_image_tensors, train_target_tensors)
+    train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # Train model and plot training loss progress
+    epochs = 10
+    steps = 0
+    train_losses = []
+
+    for e in tqdm(range(epochs)):
+        running_loss = 0
+        for images, labels in train_loader:
+            steps += 1
+
+            optimizer.zero_grad()
+
+            log_ps = model(images)
+            loss = criterion(log_ps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            train_losses.append(running_loss / len(train_loader))
+
+    # Save plot of training loss in current folder
+    plot_path = os.path.join(os.path.dirname(__file__), "..", "reports", "figures", "training_loss.png")
+    print(plot_path)
+    plt.plot(train_losses, label="Training loss")
+    plt.legend(frameon=False)
+    plt.savefig(plot_path)
+
+    # Save model
+    torch.save(model, os.path.join(os.path.dirname(__file__), "..", "models", "model.pt"))
+
+
+if __name__ == "__main__":
+    train(1e-3)
