db729058f1dd7c8513af57d730aa65a665c2f671
augustsemrau <augu0093@gmail.com>
2024-01-08 11:51:44 +0100

    working on predict model

--- mlops_mnist_project/predict_model.py
+++ mlops_mnist_project/predict_model.py
@@ -1,3 +1,12 @@
+"""
+Fill out the newly created <project_name>/models/predict_model.py file, such that it takes a pre-trained model file and creates prediction for some data. 
+Recommended interface is that users can give this file either a folder with raw images that gets loaded in or a numpy or pickle file with already loaded images e.g. something like this
+
+python <project_name>/models/predict_model.py \
+    models/my_trained_model.pt \  # file containing a pretrained model
+    data/example_images.npy  # file containing just 10 images for prediction
+"""
+
 import torch
 
 def predict(
@@ -14,4 +23,6 @@ def predict(
         Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
 
     """
+
+    
     return torch.cat([model(batch) for batch in dataloader], 0)
